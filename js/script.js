6f93959f9827c2dc82433812df4f1e3203624389
Paul Belfrage <30831777+paul-pauliskolan@users.noreply.github.com>
2026-03-09 19:08:39 +0100

    Implement button click event for info display
    Add event listener to button for displaying info

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+TODO:
+
+När användaren klickar på knappen ska mer information visas
+*/
+
+const button = document.getElementById("infoButton")
+
+button.addEventListener("click", function(){
+
+console.log("Knappen klickades")
+
+// TODO: visa mer text på sidan
+
+})
